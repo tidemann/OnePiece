668d6465cecaea14b1e4ae37d0eebdd3d5b2bf3a
tidemann <tidemann@gmail.com>
2014-01-22 15:14:30 +0100

    added webserver

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,35 @@ var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     livereload = require('gulp-livereload'),
     lr = require('tiny-lr'),
+    connect = require('connect'),
+    http = require('http'),
+    path = require('path'),
     clean = require('gulp-clean'),
     notify = require('gulp-notify'),
     server = lr();
 
 gulp.task('default', ['clean'], function() {
-    gulp.run('styles');
+    gulp.run('styles', 'webserver', 'watch');
 });
+
+gulp.task('webserver', function() {
+    var port = 3000,
+        hostname = null,
+        base = path.resolve('.'),
+        directory = path.resolve('.'),
+        app = connect().use(connect["static"](base)).use(connect.directory(directory));
+    return http.createServer(app).listen(port, hostname);
+});
+
 gulp.task('styles', function() {
     return gulp.src('src/styles/main.scss')
         .pipe(sass({
             style: 'expanded'
         }))
         .pipe(gulp.dest('dist/assets/css'))
-        .pipe(livereload(server))
-        .pipe(notify({
-            message: 'Syles task complete'
-        }));
+        .pipe(livereload(server));
 });
+
 gulp.task('clean', function() {
     return gulp.src(['dist/assets/css'], {
         read: false
@@ -37,5 +48,4 @@ gulp.task('watch', function() {
             gulp.run('styles');
         });
     });
-
 });
